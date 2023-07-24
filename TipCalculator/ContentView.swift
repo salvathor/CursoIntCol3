0358ff6b67a812b670c8ebfefe59d2fa067b0533
Salvador Lopez <salvatore.isc@gmail.com>
2023-07-24 09:19:36 -0600

    Tip Calculator - Working

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -8,22 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var billAmount = 0
-    @State var tipPercentage: Double = 5
-    @State var tipAmount = 0
-    @State var totalAmount = 0
+    @StateObject var viewModel = TipCalculatorModel()
     var body: some View {
         VStack {
-            TextField("Enter bill amount", value: $billAmount, formatter: NumberFormatter())
+            TextField("Enter bill amount", value: $viewModel.billAmount, formatter: NumberFormatter())
                 .textFieldStyle(.roundedBorder)
                 .padding()
-            Slider(value: $tipPercentage, in: 0...30, step: 5)
+            Slider(value: $viewModel.tipPercentage, in: 0...30, step: 5)
                 .padding()
-            Text("Tip percentage: \(Int(tipPercentage))%")
+            Text("Tip percentage: \(Int(viewModel.tipPercentage))%")
                 .font(.title3)
-            Text("Tip amount: $\(tipAmount, specifier: "%.2f")")
+            Text("Tip amount: $\(viewModel.tipAmount, specifier: "%.2f")")
                 .font(.title3)
-            Text("Total amount: \(totalAmount, specifier: "%.2f")")
+            Text("Total amount: \(viewModel.totalAmount, specifier: "%.2f")")
                 .font(.title2)
         }
         .padding()
