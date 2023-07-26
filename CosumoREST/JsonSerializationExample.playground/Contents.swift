7ca9f596c93a6e5b61f53d8d38c3a1f0c2045e80
Salvador Lopez <salvatore.isc@gmail.com>
2023-07-26 06:25:10 -0600

    JSONSerialization Object Examples

---
+++ CosumoREST/JsonSerializationExample.playground/Contents.swift
@@ -0,0 +1,120 @@
+import UIKit
+
+let student = [
+    "name":"Rogelio",
+    "age":6
+] as [String:AnyObject]
+
+JSONSerialization.isValidJSONObject(student)
+
+do{
+    // Creando el obj json a partir de un obj foundation.
+    let data = try JSONSerialization.data(withJSONObject: student)
+    // Visualizar el obj JSON como un str.
+    let jsonString = String(data: data, encoding: .utf8)
+    //JSONSerialization.isValidJSONObject(jsonString)
+    let studentDictionary = try JSONSerialization.jsonObject(with: data)
+}catch{
+    print("Error: \(error)")
+}
+
+// var myDouble =  3.14159
+// String(format: "%.2f", myDouble)
+// \(myDouble, specifier: "%.2f")
+
+let jsonString = """
+{
+
+  "squadName": "Super hero squad",
+
+  "homeTown": "Metro City",
+
+  "formed": 2016,
+
+  "secretBase": "Super tower",
+
+  "active": true,
+
+  "members": [
+
+    {
+
+      "name": "Molecule Man",
+
+      "age": 29,
+
+      "secretIdentity": "Dan Jukes",
+
+      "powers": [
+
+        "Radiation resistance",
+
+        "Turning tiny",
+
+        "Radiation blast"
+
+      ]
+
+    },
+
+    {
+
+      "name": "Madame Uppercut",
+
+      "age": 39,
+
+      "secretIdentity": "Jane Wilson",
+
+      "powers": [
+
+        "Million tonne punch",
+
+        "Damage resistance",
+
+        "Superhuman reflexes"
+
+      ]
+
+    },
+
+    {
+
+      "name": "Eternal Flame",
+
+      "age": 1000000,
+
+      "secretIdentity": "Unknown",
+
+      "powers": [
+
+        "Immortality",
+
+        "Heat Immunity",
+
+        "Inferno",
+
+        "Teleportation",
+
+        "Interdimensional travel"
+
+      ]
+
+    }
+
+  ]
+
+}
+"""
+
+if let jsonData = jsonString.data(using: .utf8){
+    do {
+        if let jsonObject = try JSONSerialization.jsonObject(with: jsonData) as? [String:Any]{
+            let foundationObj = NSDictionary(dictionary: jsonObject)
+            print(foundationObj)
+            print(type(of: foundationObj))
+            print(foundationObj["squadName"] ?? "No se encontro ningun elemento con el indice /'squadName/'")
+        }
+    }catch{
+        print("Error: \(error)")
+    }
+}
